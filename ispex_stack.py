fa55a71d2f15e9faf0f3b02aa0d34947ee87649d
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2018-09-10 12:08:23 +0200

    Stack as function

--- ispex_stack.py
+++ ispex_stack.py
@@ -2,20 +2,28 @@ import pol
 import numpy as np
 from matplotlib import pyplot as plt
 
-wavelengths = np.arange(297, 802, 2)
-source = pol.Stokes_nm(np.ones_like(wavelengths), 1, 0, 0)
+wavelengths = np.arange(297, 802, 0.3)
+source = pol.Stokes_nm(np.ones_like(wavelengths), 0.7, 0, 0)
 
-QWP = pol.Retarder_wavelengths(140, 0, wavelengths)
-MOR1= pol.Retarder_wavelengths(2240, 45, wavelengths)
-MOR2= pol.Retarder_wavelengths(2240, 45, wavelengths)
-POL = pol.LinPol_deg(0)
+def simulate_iSPEX(wavelengths, source, QWP_d=140., QWP_t=0., MOR1_d=2240., MOR1_t=45., MOR2_d=2240., MOR2_t=45., POL_t=0.):
+    QWP = pol.Retarder_wavelengths(QWP_d, QWP_t, wavelengths)
+    MOR1= pol.Retarder_wavelengths(MOR1_d, MOR1_t, wavelengths)
+    MOR2= pol.Retarder_wavelengths(MOR2_d, MOR2_t, wavelengths)
+    POL = pol.LinPol_deg(POL_t)
 
-after_QWP = np.einsum("wij,wj->wi", QWP, source)
-after_MOR1= np.einsum("wij,wj->wi", MOR1, after_QWP)
-after_MOR2= np.einsum("wij,wj->wi", MOR2, after_MOR1)
-after_POL = np.einsum( "ij,wj->wi", POL, after_MOR2)
+    after_QWP = np.einsum("wij,wj->wi", QWP , source    )
+    after_MOR1= np.einsum("wij,wj->wi", MOR1, after_QWP )
+    after_MOR2= np.einsum("wij,wj->wi", MOR2, after_MOR1)
+    after_POL = np.einsum( "ij,wj->wi", POL , after_MOR2)
 
-plt.plot(wavelengths, after_POL[:,0])
-plt.xlim(300, 800)
-plt.ylim(0, 1)
-plt.show()
+    return after_POL.T  # transpose to split I, Q, U, V if wanted
+
+I, Q, U, V = simulate_iSPEX(wavelengths, source)
+
+fig, ax = plt.subplots(figsize=(10,5), tight_layout=True)
+ax.plot(wavelengths, I, c='k')
+ax.set_xlim(300, 800)
+ax.set_ylim(0, 1)
+ax.set_xlabel("$\lambda$ (nm)")
+ax.set_ylabel("Stokes $I$")
+fig.show()
