2ab4ed4d20db5ed9d4d65fa84b40cba6202b8b1b
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2019-01-30 15:32:23 +0100

    Split off optical elements & Stokes calculus

--- spex/optics/elements.py
+++ spex/optics/elements.py
@@ -1,33 +1,6 @@
 import numpy as np
 from numpy import sin, cos
 
-def DoP(I, Q, U, V):
-    return np.sqrt(Q**2 + U**2 + V**2) / I
-
-def DoLP(I, Q, U, V):
-    return DoP(I, Q, U, V=0.)
-
-def AoLP(I, Q, U, V):
-    return 0.5 * np.arctan2(U, Q)
-
-def AoLP_deg(I, Q, U, V):
-    return np.rad2deg(AoLP(I, Q, U, V))
-
-def Stokes(I=0., Q=0., U=0., V=0.):
-    assert Q**2 + U**2 + V**2 <= I**2
-    return np.array([I,Q,U,V], dtype=np.float64)[:,np.newaxis]
-
-def Stokes_from_pol(I0=0, I90=0, I45=0, Im45=0, ILHC=0, IRHC=0):
-    return Stokes(I0+I90, I0-I90, I45-Im45, ILHC-IRHC)
-
-def Stokes_nm(I=0., Q=0., U=0., V=0.):
-    arr = np.zeros((len(I), 4))
-    arr[:,0] = I
-    arr[:,1] = Q  # split to allow int/float Q/U/V values
-    arr[:,2] = U
-    arr[:,3] = V
-    return arr
-
 def Rotation_matrix_radians(phi):
     return np.array([[1.,          0.,          0., 0.],
                      [0.,  cos(2*phi),  sin(2*phi), 0.],
