5f2bce5ae212057ba998227005f94bce1a32c845
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2020-09-14 16:46:13 +0200

    Label location and formatting

--- foil_colour.py
+++ foil_colour.py
@@ -13,21 +13,24 @@ polariser_90 = elements.Linear_polarizer_degrees(90)
 # Propagate the source light through the input polariser
 after_polariser_0 = np.einsum("ij,wj->wi", polariser_0, source)
 
-# Create the foil
-retardance = 10*560. # nm
-retardance_560 = retardance / 560. # retardance in lambda units at 560 nm
-label = f"Retardance:\n{retardance:.0f} nm\n{retardance_560:4.1f} $\lambda$"
-foil = elements.Retarder_wavelengths(retardance, 45, wavelengths)
+for retardance_560 in np.linspace(0, 10, 100):
+    # Create the foil
+    retardance = retardance_560*560. # nm
+    label = f"Retardance: {retardance:.0f} nm ; {retardance_560:4.1f} $\lambda$"
+    foil = elements.Retarder_wavelengths(retardance, 45, wavelengths)
 
-# Propagate the light through the foil
-after_foil = np.einsum("wij,wj->wi", foil, after_polariser_0)
+    # Propagate the light through the foil
+    after_foil = np.einsum("wij,wj->wi", foil, after_polariser_0)
 
-# Propagate the light through the output polariser
-after_polariser_90 = np.einsum("ij,wj->wi", polariser_90, after_foil)
+    # Propagate the light through the output polariser
+    after_polariser_90 = np.einsum("ij,wj->wi", polariser_90, after_foil)
 
-plt.plot(wavelengths, after_polariser_90[:,0], label=label)
-plt.xlabel("Wavelength [nm]")
-plt.ylabel("Intensity [a.u.]")
-plt.legend(loc="upper right", bbox_to_anchor=(1.32,1))
-plt.show()
-plt.close()
+    plt.figure(figsize=(5,4))
+    plt.plot(wavelengths, after_polariser_90[:,0])
+    plt.xlabel("Wavelength [nm]")
+    plt.ylabel("Intensity [a.u.]")
+    plt.annotate(label, xy=(0.1,0.9), xycoords="figure fraction")
+    plt.savefig(f"animation/{retardance:07.2f}.png")
+    plt.close()
+
+    print(retardance, "nm")
