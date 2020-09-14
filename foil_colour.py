3fdda9481d65aa6aefb62a978462028e8fa1b9bc
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2020-09-14 16:30:49 +0200

    Retardance in legend

--- foil_colour.py
+++ foil_colour.py
@@ -14,7 +14,9 @@ polariser_90 = elements.Linear_polarizer_degrees(90)
 after_polariser_0 = np.einsum("ij,wj->wi", polariser_0, source)
 
 # Create the foil
-retardance = 10*560. #nm
+retardance = 10*560. # nm
+retardance_560 = retardance / 560. # retardance in lambda units at 560 nm
+label = f"Retardance:\n{retardance:.0f} nm\n{retardance_560:4.1f} $\lambda$"
 foil = elements.Retarder_wavelengths(retardance, 45, wavelengths)
 
 # Propagate the light through the foil
@@ -23,9 +25,9 @@ after_foil = np.einsum("wij,wj->wi", foil, after_polariser_0)
 # Propagate the light through the output polariser
 after_polariser_90 = np.einsum("ij,wj->wi", polariser_90, after_foil)
 
-plt.plot(wavelengths, after_polariser_90[:,0])
+plt.plot(wavelengths, after_polariser_90[:,0], label=label)
 plt.xlabel("Wavelength [nm]")
 plt.ylabel("Intensity [a.u.]")
-plt.title(f"Retardance: {retardance:.0f} nm, {retardance/560:.1f} $\lambda$ at 560 nm")
+plt.legend(loc="upper right", bbox_to_anchor=(1.32,1))
 plt.show()
 plt.close()
