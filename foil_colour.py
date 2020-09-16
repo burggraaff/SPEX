92275030f6640f9e3c0a0795599ca8d0ed637f6c
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2020-09-16 15:04:20 +0200

    Remove propagation from loop

--- foil_colour.py
+++ foil_colour.py
@@ -30,29 +30,26 @@ polariser_90 = elements.Linear_polarizer_general(0.9, 0.005, 90)
 after_polariser_0 = np.einsum("ij,wi->wj", polariser_0, source)
 
 # Retardances to loop over
-retardances = np.linspace(0, 10, 250)
+retardances_relative = np.linspace(0, 10, 250)
+retardances_absolute = retardances_relative * 560.
 
 # Integral of RGB intensities
-integral_SRF = np.zeros((3,len(retardances)))
-statistic = np.zeros_like(retardances)
+integral_SRF = np.zeros((3,len(retardances_relative)))
+statistic = np.zeros_like(retardances_relative)
 
-# foils = np.stack([elements.Retarder_wavelengths(retardance, 45, wavelengths) for retardance in retardances], axis=-1)
-# after_foils = np.einsum("wijr,wi->wjr", foils, after_polariser_0)
+# Create foils and propagate through them
+foils = np.stack([elements.Retarder_wavelengths(retardance_absolute, 45, wavelengths) for retardance_absolute in retardances_absolute])
+after_foils = np.einsum("rwij,wi->rwj", foils, after_polariser_0)
 
-for i, retardance_560 in enumerate(retardances):
-    # Create the foil
-    retardance = retardance_560*560. # nm
-    label = f"Retardance: {retardance:.0f} nm ; {retardance_560:4.1f} $\lambda$"
-    foil = elements.Retarder_wavelengths(retardance, 45, wavelengths)
+# Propagate through the output polariser
+after_polariser_90 = np.einsum("ij,rwj->rwi", polariser_90, after_foils)
 
-    # Propagate the light through the foil
-    after_foil = np.einsum("wij,wi->wj", foil, after_polariser_0)
+# Get only the total intensity spectra
+intensities = after_polariser_90[...,0]
 
-    # Propagate the light through the output polariser
-    after_polariser_90 = np.einsum("ij,wj->wi", polariser_90, after_foil)
-
-    # Get the unpolarised intensity
-    intensity = after_polariser_90[:,0]
+for i, (retardance_relative, retardance_absolute, intensity) in enumerate(zip(retardances_relative, retardances_absolute, intensities)):
+    # Status indicator
+    label = f"Retardance: {retardance_absolute:.0f} nm ; {retardance_relative:4.1f} $\lambda$"
 
     plt.figure(figsize=(5,4))
     plt.plot(wavelengths, intensity)
@@ -61,7 +58,7 @@ for i, retardance_560 in enumerate(retardances):
     plt.xlabel("Wavelength [nm]")
     plt.ylabel("Intensity [a.u.]")
     plt.annotate(label, xy=(0.1,0.9), xycoords="figure fraction")
-    plt.savefig(f"animation/raw_spectrum_{retardance:07.2f}.png")
+    plt.savefig(f"animation/raw_spectrum_{retardance_absolute:07.2f}.png")
     plt.close()
 
     # Get the intensity in RGB
@@ -75,31 +72,31 @@ for i, retardance_560 in enumerate(retardances):
     plt.xlabel("Wavelength [nm]")
     plt.ylabel("Intensity [a.u.]")
     plt.annotate(label, xy=(0.1,0.9), xycoords="figure fraction")
-    plt.savefig(f"animation/RGB_spectrum_{retardance:07.2f}.png")
+    plt.savefig(f"animation/RGB_spectrum_{retardance_absolute:07.2f}.png")
     plt.close()
 
     integral_SRF[:,i] = np.trapz(intensity_SRF, x=wavelengths, axis=1)
 
     statistic[i] = np.nanmean(gauss1d(intensity, sigma=5))
 
-    print(retardance, "nm")
+    print(retardance_absolute, "nm")
 
 for j, c in enumerate("rgb"):
-    plt.plot(retardances, integral_SRF[j], c=c)
+    plt.plot(retardances_relative, integral_SRF[j], c=c)
 plt.xlabel("Retardance in $\lambda$ at 560 nm")
 plt.ylabel("RGB value")
 plt.savefig("retardance_RGB.pdf", bbox_inches="tight")
 plt.show()
 plt.close()
 
-plt.plot(retardances, np.rad2deg(np.arctan2(integral_SRF[0], integral_SRF[2])))
+plt.plot(retardances_relative, np.rad2deg(np.arctan2(integral_SRF[0], integral_SRF[2])))
 plt.xlabel("Retardance in $\lambda$ at 560 nm")
 plt.ylabel("R/B hue angle [degrees]")
 plt.savefig("retardance_hue.pdf", bbox_inches="tight")
 plt.show()
 plt.close()
 
-plt.plot(retardances, statistic)
+plt.plot(retardances_relative, statistic)
 plt.xlabel("Retardance in $\lambda$ at 560 nm")
 plt.ylabel("Statistic")
 plt.savefig("retardance_stat.pdf", bbox_inches="tight")
