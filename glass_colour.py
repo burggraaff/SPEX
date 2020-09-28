bf809f3af50bc964027bd2bd22bb0678ce9e87ad
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2020-09-28 14:40:38 +0200

    Gamut plot

--- glass_colour.py
+++ glass_colour.py
@@ -3,6 +3,7 @@ from matplotlib import pyplot as plt
 from spex import stokes, elements
 from spectacle.general import gauss1d, gaussMd
 from spectacle import spectral
+from colorio._tools import plot_flat_gamut
 
 # Load the input spectrum
 input_spectrum = np.loadtxt("input_data/lamp_spectrum.txt", skiprows=14)
@@ -99,3 +100,39 @@ plt.legend(loc="best")
 plt.savefig("retardance_sRGB_clipped.pdf", bbox_inches="tight")
 plt.show()
 plt.close()
+
+for i, (retardance_relative, retardance_absolute, intensity, xy, sRGB) in enumerate(zip(retardances_relative, retardances_absolute, intensities, intensity_xy.T, intensity_sRGB_clip.T)):
+    # Status indicator
+    label = f"Retardance: {retardance_absolute:5.0f} nm ; {retardance_relative:4.1f} $\lambda$"
+
+    # Make plots
+    fig, axs = plt.subplots(ncols=3, figsize=(8,3))
+
+    # First panel: intensity spectrum
+    axs[0].plot(wavelengths, intensity)
+    axs[0].set_xlim(380, 720)
+    axs[0].set_ylim(-0.02, 0.5)
+    axs[0].set_xlabel("Wavelength [nm]")
+    axs[0].set_ylabel("Intensity [a.u.]")
+    axs[0].set_title(f"Retardance:\n{retardance_absolute:5.0f} nm    {retardance_relative:4.1f} $\lambda$")
+
+    # Second panel: gamut
+    plt.sca(axs[1])
+    plot_flat_gamut(plot_planckian_locus=False, axes_labels=("", ""))
+    plt.plot(*intensity_xy[:,:i+1], c='k')
+    plt.scatter(*xy, c='k')
+    axs[1].set_title(f"$(x, y) = ({xy[0]:.2f}, {xy[1]:.2f})$")
+
+    # Third panel: colour
+    rect = plt.Rectangle((0, 0), 1, 1, facecolor=sRGB)
+    axs[2].add_patch(rect)
+    axs[2].tick_params(left=False, labelleft=False, bottom=False, labelbottom=False)
+    axs[2].set_aspect("equal")
+    axs[2].set_title(f"$(r, g, b) = ({sRGB[0]:.2f}, {sRGB[1]:.2f}, {sRGB[2]:.2f})$")
+
+
+    plt.savefig(f"animation/gamut_{retardance_absolute:07.2f}.png")
+    plt.show()
+    plt.close()
+
+    print(label)
