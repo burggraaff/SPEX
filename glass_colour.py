adcc41b14a011c6afb94a3c0eb8608a547acbaab
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2020-09-30 01:18:10 +0200

    Parallel/Orthogonal polarisers

--- glass_colour.py
+++ glass_colour.py
@@ -38,37 +38,49 @@ integral_SRF = np.zeros((3,len(retardances_relative)))
 foils = np.stack([elements.Retarder_wavelengths(retardance_absolute, 45, wavelengths) for retardance_absolute in retardances_absolute])
 after_foils = np.einsum("rwij,wj->rwi", foils, after_polariser_0)
 
-# Propagate through the output polariser
-after_polariser_90 = np.einsum("ij,rwj->rwi", polariser_90, after_foils)
+# Propagate through the output polarisers (orthogonal, parallel)
+after_polariser_orthogonal = np.einsum("ij,rwj->rwi", polariser_90, after_foils)
+after_polariser_parallel = np.einsum("ij,rwj->rwi", polariser_0, after_foils)
+polariser_labels = ["Orthogonal", "Parallel"]
 
 # Get only the total intensity spectra
-intensities = after_polariser_90[...,0]
+intensities_orthogonal = after_polariser_orthogonal[...,0]
+intensities_parallel = after_polariser_parallel[...,0]
 
 # Interpolate the intensity spectra to the CIE XYZ wavelengths
-intensity_interpolated = spectral.interpolate_spectral_data(wavelengths, intensities, spectral.cie_wavelengths)
+intensity_orthogonal_interpolated = spectral.interpolate_spectral_data(wavelengths, intensities_orthogonal, spectral.cie_wavelengths)
+intensity_parallel_interpolated = spectral.interpolate_spectral_data(wavelengths, intensities_parallel, spectral.cie_wavelengths)
 
 # Convert the intensity spectra to CIE XYZ
-intensity_XYZ = np.einsum("xw,rw->xr", spectral.cie_xyz, intensity_interpolated) / len(spectral.cie_wavelengths)
+intensity_orthogonal_XYZ = np.einsum("xw,rw->xr", spectral.cie_xyz, intensity_orthogonal_interpolated) / len(spectral.cie_wavelengths)
+intensity_parallel_XYZ = np.einsum("xw,rw->xr", spectral.cie_xyz, intensity_parallel_interpolated) / len(spectral.cie_wavelengths)
 
 # Plot XYZ as a function of retardance
-for XYZ, label in zip(intensity_XYZ, "XYZ"):
-    plt.plot(retardances_relative, XYZ, label=label, lw=3)
-plt.xlabel("Retardance in $\lambda$ at 560 nm")
-plt.ylabel("CIE XYZ")
-plt.legend(loc="best")
+fig, axs = plt.subplots(nrows=2, sharex=True, sharey=True)
+for ax, intensity_XYZ, pol_label in zip(axs, [intensity_orthogonal_XYZ, intensity_parallel_XYZ], polariser_labels):
+    for XYZ, label in zip(intensity_XYZ, "XYZ"):
+        ax.plot(retardances_relative, XYZ, label=label, lw=3)
+    ax.set_ylabel(f"CIE XYZ ({pol_label})")
+    ax.legend(ncol=3, loc="lower right")
+    ax.grid(ls="--")
+axs[1].set_xlabel("Retardance in $\lambda$ at 560 nm")
 plt.savefig("retardance_XYZ.pdf", bbox_inches="tight")
 plt.show()
 plt.close()
 
 # Convert to xy chromaticity
-intensity_xy = (intensity_XYZ / intensity_XYZ.sum(axis=0))[:2]
+intensity_orthogonal_xy = (intensity_orthogonal_XYZ / intensity_orthogonal_XYZ.sum(axis=0))[:2]
+intensity_parallel_xy = (intensity_parallel_XYZ / intensity_parallel_XYZ.sum(axis=0))[:2]
 
 # Plot xy as a function of retardance
-for xy, label in zip(intensity_xy, "xy"):
-    plt.plot(retardances_relative, xy, label=label, lw=3)
-plt.xlabel("Retardance in $\lambda$ at 560 nm")
-plt.ylabel("CIE xy (normalised)")
-plt.legend(loc="best")
+fig, axs = plt.subplots(nrows=2, sharex=True, sharey=True)
+for ax, intensity_xy, pol_label in zip(axs, [intensity_orthogonal_xy, intensity_parallel_xy], polariser_labels):
+    for xy, label in zip(intensity_xy, "xy"):
+        ax.plot(retardances_relative, xy, label=label, lw=3)
+    ax.set_ylabel(f"CIE xy ({pol_label})")
+    ax.grid(ls="--")
+    ax.legend(ncol=2, loc="lower right")
+axs[1].set_xlabel("Retardance in $\lambda$ at 560 nm")
 plt.savefig("retardance_xy.pdf", bbox_inches="tight")
 plt.show()
 plt.close()
@@ -80,59 +92,70 @@ M_rgb_to_xyz = np.array([[0.4124564, 0.3575761, 0.1804375],
 
 M_xyz_to_rgb = np.linalg.inv(M_rgb_to_xyz)
 
-intensity_XYZ_normalised = intensity_XYZ * 7
-intensity_sRGB = M_xyz_to_rgb @ intensity_XYZ_normalised
+normalisation = 7.  # Arbitrary, controls brightness of resulting colours
+intensity_orthogonal_sRGB = M_xyz_to_rgb @ intensity_orthogonal_XYZ * normalisation
+intensity_parallel_sRGB = M_xyz_to_rgb @ intensity_parallel_XYZ * normalisation
 
 # Plot sRGB as a function of retardance
-for sRGB, label in zip(intensity_sRGB, "rgb"):
-    plt.plot(retardances_relative, sRGB, label=label, lw=3, c=label)
-plt.xlabel("Retardance in $\lambda$ at 560 nm")
-plt.ylabel("sRGB")
-plt.legend(loc="best")
+fig, axs = plt.subplots(nrows=2, sharex=True, sharey=True)
+for ax, intensity_sRGB, pol_label in zip(axs, [intensity_orthogonal_sRGB, intensity_parallel_sRGB], polariser_labels):
+    for sRGB, label in zip(intensity_sRGB, "rgb"):
+        ax.plot(retardances_relative, sRGB, label=label, lw=3, c=label)
+    ax.set_ylabel(f"sRGB ({pol_label})")
+    ax.grid(ls="--")
+    ax.legend(ncol=3, loc="lower right")
+axs[1].set_xlabel("Retardance in $\lambda$ at 560 nm")
 plt.savefig("retardance_sRGB.pdf", bbox_inches="tight")
 plt.show()
 plt.close()
 
 # Clip sRGB and plot as a function of retardance
-intensity_sRGB_clip = np.clip(intensity_sRGB, 0, 1)
-for sRGB, label in zip(intensity_sRGB_clip, "rgb"):
-    plt.plot(retardances_relative, sRGB, label=label, lw=3, c=label)
-plt.xlabel("Retardance in $\lambda$ at 560 nm")
-plt.ylabel("sRGB")
-plt.legend(loc="best")
+intensity_orthogonal_sRGB_clip = np.clip(intensity_orthogonal_sRGB, 0, 1)
+intensity_parallel_sRGB_clip = np.clip(intensity_parallel_sRGB, 0, 1)
+fig, axs = plt.subplots(nrows=2, sharex=True, sharey=True)
+for ax, intensity_sRGB, pol_label in zip(axs, [intensity_orthogonal_sRGB_clip, intensity_parallel_sRGB_clip], polariser_labels):
+    for sRGB, label in zip(intensity_sRGB, "rgb"):
+        ax.plot(retardances_relative, sRGB, label=label, lw=3, c=label)
+    ax.set_ylabel(f"sRGB ({pol_label})")
+    ax.grid(ls="--")
+    ax.legend(ncol=3, loc="lower right")
+axs[1].set_xlabel("Retardance in $\lambda$ at 560 nm")
 plt.savefig("retardance_sRGB_clipped.pdf", bbox_inches="tight")
 plt.show()
 plt.close()
 
-for i, (retardance_relative, retardance_absolute, intensity, xy, sRGB) in enumerate(zip(retardances_relative, retardances_absolute, intensities, intensity_xy.T, intensity_sRGB_clip.T)):
+for i, (retardance_relative, retardance_absolute, intensity_orthogonal, intensity_parallel, xy_orthogonal, xy_parallel, sRGB_orthogonal, sRGB_parallel) in enumerate(zip(retardances_relative, retardances_absolute, intensities_orthogonal, intensities_parallel, intensity_orthogonal_xy.T, intensity_parallel_xy.T, intensity_orthogonal_sRGB_clip.T, intensity_parallel_sRGB_clip.T)):
     # Status indicator
     label = f"Retardance: {retardance_absolute:5.0f} nm ; {retardance_relative:4.1f} $\lambda$"
 
     # Make plots
-    fig, axs = plt.subplots(ncols=3, figsize=(8,3))
+    fig, axs = plt.subplots(ncols=3, nrows=2, figsize=(8,6), sharex="col", sharey="col")
 
     # First panel: intensity spectrum
-    axs[0].plot(wavelengths, intensity)
-    axs[0].set_xlim(380, 720)
-    axs[0].set_ylim(-0.02, 0.5)
-    axs[0].set_xlabel("Wavelength [nm]")
-    axs[0].set_ylabel("Intensity [a.u.]")
-    axs[0].set_title(f"Retardance:\n{retardance_absolute:5.0f} nm    {retardance_relative:4.1f} $\lambda$")
+    for ax, intensity, pol_label in zip(axs[:,0], [intensity_orthogonal, intensity_parallel], polariser_labels):
+        ax.plot(wavelengths, intensity)
+        ax.set_xlim(380, 720)
+        ax.set_ylim(-0.02, 0.5)
+        ax.set_ylabel(f"{pol_label}\nIntensity [a.u.]")
+
+    axs[0,0].set_title(f"Retardance:\n{retardance_absolute:5.0f} nm    {retardance_relative:4.1f} $\lambda$")
+    axs[1,0].set_xlabel("Wavelength [nm]")
 
     # Second panel: gamut
-    plt.sca(axs[1])
-    plot_flat_gamut(plot_planckian_locus=False, axes_labels=("", ""))
-    plt.plot(*intensity_xy[:,:i+1], c='k')
-    plt.scatter(*xy, c='k')
-    axs[1].set_title(f"$(x, y) = ({xy[0]:.2f}, {xy[1]:.2f})$")
+    for ax, intensity_xy, xy in zip(axs[:,1], [intensity_orthogonal_xy, intensity_parallel_xy], [xy_orthogonal, xy_parallel]):
+        plt.sca(ax)
+        plot_flat_gamut(plot_planckian_locus=False, axes_labels=("", ""))
+        plt.plot(*intensity_xy[:,:i+1], c='k')
+        plt.scatter(*xy, c='k')
+        ax.set_title(f"$(x, y) = ({xy[0]:.2f}, {xy[1]:.2f})$")
 
     # Third panel: colour
-    rect = plt.Rectangle((0, 0), 1, 1, facecolor=sRGB)
-    axs[2].add_patch(rect)
-    axs[2].tick_params(left=False, labelleft=False, bottom=False, labelbottom=False)
-    axs[2].set_aspect("equal")
-    axs[2].set_title(f"$(r, g, b) = ({sRGB[0]:.2f}, {sRGB[1]:.2f}, {sRGB[2]:.2f})$")
-
+    for ax, sRGB in zip(axs[:,2], [sRGB_orthogonal, sRGB_parallel]):
+        rect = plt.Rectangle((0, 0), 1, 1, facecolor=sRGB)
+        ax.add_patch(rect)
+        ax.tick_params(left=False, labelleft=False, bottom=False, labelbottom=False)
+        ax.set_aspect("equal")
+        ax.set_title(f"$(r, g, b) = ({sRGB[0]:.2f}, {sRGB[1]:.2f}, {sRGB[2]:.2f})$")
 
     plt.savefig(f"animation/gamut_{retardance_absolute:07.2f}.png")
     plt.show()
