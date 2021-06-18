3975753a4b863c805584a7bb89ff73fdfdda70aa
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2021-06-18 15:01:19 +0200

    groundSPEX demodulation start

---
+++ demod_gspex.py
@@ -0,0 +1,11 @@
+"""
+Demodulation pipeline for groundSPEX data
+"""
+
+import numpy as np
+from matplotlib import pyplot as plt
+from pathlib import Path
+from sys import argv
+
+data_folder = Path(argv[1])
+
