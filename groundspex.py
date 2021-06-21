4cd692f09fa7c07a9a5d73fde35aca07c5386ce7
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2021-06-22 01:49:39 +0200

    Start of demodulation - wavelength selection

--- groundspex.py
+++ groundspex.py
@@ -210,3 +210,16 @@ def correct_efficiency(data, wavelengths=None, efficiency_data=None):
     ind_660 = np.nanargmin(np.abs(efficiency_data[0,5] - 660))
     ind_672 = np.nanargmin(np.abs(efficiency_data[0,5] - 672))
     wvlrange = np.s_[ind_660:ind_672]
+
+
+def demodulate(data, wavelengths=None, wavelength_range=[370., 800.]):
+    """
+    Main demodulation routine.
+    """
+    if wavelengths is None:
+        wavelengths = generate_wavelengths()
+
+    # Select the relevant wavelengths
+    wvl = np.where((wavelengths > wavelength_range[0]) & (wavelengths < wavelength_range[1]))[0]
+    wavelengths, data = wavelengths[wvl], data[...,wvl]
+
