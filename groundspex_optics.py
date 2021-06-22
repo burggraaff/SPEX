a55b0e21c5b249af3fae2c318a19a733d3b9fe99
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2021-06-22 03:04:08 +0200

    Add placeholders/To do

--- groundspex_optics.py
+++ groundspex_optics.py
@@ -38,6 +38,7 @@ class Material(object):
         """
         Calculate the refractive index at given wavelengths and temperature using
         the Sellmeier equation.
+
         Sellmeier equation: N[lambda] = SQRT(P1 + P2*lambda^2 / (lambda^2 - P3) $
                                          + P4*lambda^2 / (lambda^2 - P5) $
                                          + ...
@@ -64,14 +65,25 @@ class Material(object):
         # Temperature correction
         dT = temperature - self.data_dndT[:,0]
 
-        # Gosh dndT equation
-        Econv = 1e9 * 6.626e-34 * 2.9979e8 / 1.6e-19
-        wavelengths_ig = Econv / self.data_dndT[:,4]
-        RoRe = wavelengths_um**2 / (wavelengths_um**2 - wavelengths_ig[:,np.newaxis]**2)
-        dn_abs_dT = (self.data_dndT[:,2,np.newaxis] * RoRe + self.data_dndT[:,3,np.newaxis] * RoRe**2).T / (2*n)
-        n += dn_abs_dT * dT
+        if self.data_dndT[0,7] == 0:
+            # Schott's Sellmeier-type equation
+            pass
+        else:
+            # Gosh dndT equation
+            Econv = 1e9 * 6.626e-34 * 2.9979e8 / 1.6e-19
+            wavelengths_ig = Econv / self.data_dndT[:,4]
+            RoRe = wavelengths_um**2 / (wavelengths_um**2 - wavelengths_ig[:,np.newaxis]**2)
+            dn_abs_dT = (self.data_dndT[:,2,np.newaxis] * RoRe + self.data_dndT[:,3,np.newaxis] * RoRe**2).T / (2*n)
+            n += dn_abs_dT * dT
 
         return n
+
+
+    def retardance(self, wavelengths, temperature):
+        """
+        Calculate the retardance of this material at given wavelengths and temperature.
+        """
+        pass
 
 
 Al2O3 = Material(name="Al2O3/Sapphire",
@@ -93,7 +105,7 @@ MgF2 = Material(name="MgF2",
                 data_TEC = np.array([9.3e-6, 14.2e-6]),
                 source = "Dodge 1984, Applt. Opt. 23")
 
-SiO2 = Material(name="SiO2",
+SiO2 = Material(name="SiO2/Quartz",
                 data_n = np.array([[1.28604141, 1.07044083, 1.00585997*0.01, 1.10202242, 100.],
                                    [1.28851804, 1.09509924, 1.02101864*0.01, 1.15662475, 100.]]),
                 data_dndT = np.array([[293., 1.515, -61.184E-06,  43.9990E-06, 10.30, 8.90,  6.88, -3.02],
@@ -101,3 +113,50 @@ SiO2 = Material(name="SiO2",
                 data_transmission = None,
                 data_TEC = np.array([6.88e-6, 12.38e-6]),
                 source = "Gosh 1999 (Halle)")
+
+# ; beta-BaB2O4, BBO, 0.22 - 1.06 micron - dn/dT values of beta-BBO
+# ; Eimerl, D., Davis, L., and Velsko, S., Optical, mechanical, and thermal properties of barium borate,
+# ; J. Appl. Phys. 62, 1968 (1987).
+# ; Ghosh, G, Temperature dispersion of refractive indices in βBaB2O4 and LiB3O5 crystals for nonlinear optical devices
+# ; J. Appl. Phys. 78, 6752 (1995)
+# optics[1].N[0:4,0] = [1., 1.73651, 0.0120649, 0.0758505, 264.897]
+# optics[1].N[0:4,1] = [1., 1.36847, 0.0100179,   1.29495, 187.560]
+# optics[1].dNdT[*,0] = [293., 1.610, -19.3007E-6, -34.9683E-6, 19.00, 6.43,  4.00,  1.40]
+# optics[1].dNdT[*,1] = [293., 1.520, -141.421E-6, 110.8630E-6, 17.00, 6.43, 36.00, -5.40]
+# optics[1].tec = 1E-6*[4.00, 36.00]
+
+# ; CaCO3, Calcite, 0.2 - 2.2 micron
+# ; Gray, D. E., (Ed.), American Institute of Physics Handbook, 3rd ed. (McGraw-Hill, New York, 1972).
+# optics[2].N[0:8,0] = [1., 0.8559, 0.0588^2, 0.83913, 0.141^2, 0.0009, 0.197^2, 0.6845, 7.005^2]
+# optics[2].N[0:6,1] = [1., 1.0856, 0.07897^2, 0.0988, 0.142^2, 0.317, 1.468^2]
+# optics[2].dNdT[*,0] = [293., 1.613, -121.689E-06, 122.494E-06, 10.80, 10.00, 25.00, -7.60]
+# optics[2].dNdT[*,1] = [293., 1.472,  12.7011E-06, 20.4803E-06,  9.05,  6.83, -3.70, -1.20]
+# optics[2].tec = 1E-6*[25.00, -3.70]
+
+# ; TiO2, Rutile, 0.43 - 1.5 micron
+# ; DeVore,J. R., Refractive index of rutile and sphalerite,
+# ; J. Opt. Soc. Am. 41, 416 (1951).
+# optics[5].N[0:4,0] = [1., 4.913, 0.0, 0.2441, 0.0803]
+# optics[5].N[0:4,1] = [1., 6.097, 0.0, 0.3322, 0.0843]
+# optics[5].dNdT[*,0] = [293., 2.432, -132.253E-06, 64.5269E-06, 4.10, 3.50,  8.98, -0.46]
+# optics[5].dNdT[*,1] = [293., 2.683, -127.565E-06, 45.2141E-06, 4.10, 3.50,  6.87, -0.26]
+# optics[5].tec = 1E-6*[8.98, 6.87]
+
+# ; YVO4, 0.5 - 1.06 micron, HoOM Ref 112/113 - dn/dT values of TiO2!
+# ; Maunder, E. A. and DeShazer, L. G., Use of yttrium orthovanadate for ; polarizers,
+# ; J. Opt. Soc. Am. 61, 684A (1971).
+# ; Lomheim, T. S. and DeShazer, L. G., Optical absorption intensities
+# ; of trivalent neodymium in the uniaxial crystal yttrium orthovanadate,
+# ; J. Appl. Phys. 49, 5517 (1978).
+# optics[6].N[0:2,0] = [1., 2.7665, 0.026884]
+# optics[6].N[0:2,1] = [1., 3.5930, 0.032103]
+# optics[6].dNdT[*,0] = [293., 2.432, -132.253E-06, 64.5269E-06, 4.10, 3.50,  8.98, -0.46]
+# optics[6].dNdT[*,0] = [293., 2.683, -127.565E-06, 45.2141E-06, 4.10, 3.50,  6.87, -0.26]
+# optics[6].tec = 1E-6*[8.98, 6.87]
+
+# ; Artificial material with spectrally constant refractive index of 1.5 and birefringence of 0.01
+# optics[9].N[0,0] = [2.25]
+# optics[9].N[0,1] = [2.2801]
+# optics[9].dNdT[*,0] = [293., 1.5, 0.0E-06, 0.0E-06, 4.10, 3.50,  8.98, -0.46]
+# optics[9].dNdT[*,0] = [293., 1.51, 0.0E-06, 0.0E-06, 4.10, 3.50,  6.87, -0.26]
+# optics[9].tec = 1E-6*[0.0, 0.0]
