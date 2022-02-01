576937ea1d876bbfbe4b931f52f83997e4e7a908
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2022-02-01 17:38:44 +0100

    Add feedback

--- demod_gspex.py
+++ demod_gspex.py
@@ -8,7 +8,7 @@ from pathlib import Path
 from sys import argv
 import groundspex as G
 
-texp = 2000.
+texp = 200.
 
 # Get data folder from command-line input
 data_folder = Path(argv[1])
@@ -16,15 +16,19 @@ print(f"Loading data from {data_folder.absolute()}")
 
 # Load spectra
 data, data_dark, data_timestamps = G.load_data_folder(data_folder)
+print("Data loaded")
 
 # Dark subtraction
 data = G.correct_darkcurrent(data, data_dark)
+print("Dark subtraction done")
 
 # Wavelength calibration
 wavelengths, data = G.correct_wavelengths(data)
+print("Wavelength correction done")
 
 # Differential transmission correction
 data = G.correct_transmission(data)
+print("Transmission correction done")
 
 # Efficiency correction
 
